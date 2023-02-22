7cd3184504a31a670187aa077944a09b386c318c
JDelahunty55 <justindelahunty@gmail.com>
2023-02-22 16:28:21 -0500

    added styles.css

--- js/lib/jquery.mobile-1.4.5.js
+++ js/lib/jquery.mobile-1.4.5.js
@@ -6155,7 +6155,7 @@ $.widget( "mobile.page", {
 		//set page min-heights to be device specific
 		$.mobile.document.bind( "pageshow", function() {
 
-			// We need to wait for window.load to make sure that styles have already been rendered,
+			// We need to wait for window.load to make sure that styles.css have already been rendered,
 			// otherwise heights of external toolbars will have the wrong value
 			if ( loadDeferred ) {
 				loadDeferred.done( $.mobile.resetActivePageHeight );
@@ -10953,7 +10953,7 @@ $.widget( "mobile.popup", {
 		this._ui.container.removeClass( "ui-popup-hidden" );
 
 		if ( this.options.overlayTheme && androidBlacklist ) {
-			/* TODO: The native browser on Android 4.0.X ("Ice Cream Sandwich") suffers from an issue where the popup overlay appears to be z-indexed above the popup itself when certain other styles exist on the same page -- namely, any element set to `position: fixed` and certain types of input. These issues are reminiscent of previously uncovered bugs in older versions of Android's native browser: https://github.com/scottjehl/Device-Bugs/issues/3
+			/* TODO: The native browser on Android 4.0.X ("Ice Cream Sandwich") suffers from an issue where the popup overlay appears to be z-indexed above the popup itself when certain other styles.css exist on the same page -- namely, any element set to `position: fixed` and certain types of input. These issues are reminiscent of previously uncovered bugs in older versions of Android's native browser: https://github.com/scottjehl/Device-Bugs/issues/3
 			This fix closes the following bugs ( I use "closes" with reluctance, and stress that this issue should be revisited as soon as possible ):
 			https://github.com/jquery/jquery-mobile/issues/4816
 			https://github.com/jquery/jquery-mobile/issues/4844
